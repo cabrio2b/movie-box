5ff8a64bff2040b8554102ec56c2552808aedb12
otherKevin <kevin-cariou@hotmail.fr>
2022-05-18 12:03:36 +0200

    Commit K 18mai 12h02

---
+++ src/components/FicheFilm.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="divFicheFilm">
+    <FilmID />
+  </section>
+</template>
+
+<script>
+import FilmID from "@components/FilmID.vue";
+export default {
+  components: {
+    FilmID,
+  },
+};
+</script>
+
+<style scoped>
+/* ICI LE STYLE DE LA FICHE COMPLETE DU FILM */
+</style>
--- src/components/FilmID.vue
+++ src/components/FilmID.vue
@@ -1,3 +1,40 @@
 <template>
-  <section id="divFilmID"></section>
+  <section class="divFilmID">
+    <!-- AJOUTER LES LIENS VERS LES PAGES REAL, ACTEUR, etc DANS LES BALISES DEJA CREEES -->
+    <h3 class="classFilmTitle">
+      <a href="">{{ filmTitle }}</a>
+    </h3>
+    <img :src="filmImage" alt="affiche" />
+    <p class="classFilmReal">
+      <a href="">{{ filmReal }}</a>
+    </p>
+    <p class="classFilmActeur">
+      <a href="">{{ filmActeur }}</a>
+    </p>
+    <!-- ^ ICI, UTILISER UN "v-for" AFIN D'AFFICHER LES n ACTEURS PRINCIPAUX, n À CHOISIR -->
+    <p class="classFilmOrigine">
+      <a href="">{{ filmOrigine }}</a>
+    </p>
+    <p class="classFilmGenre">
+      <a href="">{{ filmGenre }}</a>
+    </p>
+  </section>
 </template>
+
+<script>
+export default {
+  props: {
+    filmTitle: String,
+    filmImage: String,
+    filmReal: String,
+    filmActeur: String,
+    filmOrigine: String,
+    filmGenre: String,
+    idUniqueFilm: String, // IDENTIFIANT UNIQUE DU FILM (affiché ou non sur la fiche)
+  },
+};
+</script>
+
+<style scoped>
+/* CREER ICI LES STYLE DE LA FICHE ID DU FILM*/
+</style>
