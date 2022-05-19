80d7d3e54737c7dc09c9d2ca14103b25a68d448d
cabrio2b <cabrio2b@gmail.com>
2022-05-19 09:58:35 +0200

    Ajout moduleConnection

--- src/views/ConnectPageView.vue
+++ src/views/ConnectPageView.vue
@@ -3,31 +3,38 @@
     <div class="logoContainer">
       <img src="@/assets/logoMb.webp" alt="Logo MovieBox" />
     </div>
-    <div class="connectContainer">
-      <div class="connectModul flex">
-        <div class="decoLeft"></div>
-        <div class="inputContainer">
-        <div>
-            
-        </div>
-        </div>
-        <div class="decoRight"></div>
-      </div>
-    </div>
+    <ModuleConnexion v-if="!vueModule"></ModuleConnexion>
+    <ModuleAbonnement v-if="vueModule"></ModuleAbonnement>
   </div>
 </template>
 
 <script>
+import ModuleConnexion from '@/components/ModuleConnexion.vue'
+import ModuleAbonnement from '@/components/ModuleAbonnement.vue'
 export default {
-
+  data() {
+    return {
+      changeModule: false,
+    }
+  },
+  components: {
+    ModuleConnexion,
+    ModuleAbonnement
+  }
 };
 </script>
 
 <style scoped>
+#connectContainer {
+  margin-top: 10%;
+  justify-content: center;
+  gap: 15%;
+}
 .logoContainer {
   width: 320px;
 }
 .logoContainer img {
   width: 100%;
 }
+
 </style>
